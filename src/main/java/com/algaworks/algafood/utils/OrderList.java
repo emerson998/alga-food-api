57182c0494552c3dfa0362aa26546e2e5c8b87ec
Emerson Oliveira <emersonoliveira@apoioprodesp.sp.gov.br>
2024-05-20 15:08:31 -0300

    feat: Adicionando método de ordenação em array.

---
+++ src/main/java/com/algaworks/algafood/utils/OrderList.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.utils;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.model.CidadeModel;
+
+public class OrderList {
+
+	private static void orderList(List<CidadeModel> cidades) {
+		for (int i = 0; i < cidades.size() - 1; ++i) {
+		    for (int j = i + 1; j < cidades.size(); ++j) {
+		    	CidadeModel tmp = cidades.get(i);
+		    	CidadeModel tmp2 = cidades.get(j);
+		        if (tmp.getId().compareTo(tmp2.getId()) > 0) {
+		        	CidadeModel temp = tmp;
+		        	cidades.set(i, tmp2);
+		        	cidades.set(j, temp);
+		        }
+		    }
+		}
+	}
+	
+	
+}
